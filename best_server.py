7e214003b4b30341bee1080178146c0e6470c9dd
Damodar Dahal <underscoredam@gmail.com>
2017-05-16 08:36:04 -0500

    Colors and Windows support

--- best_server.py
+++ best_server.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 import os, glob, socket, requests, time, json, subprocess
 import re
 from functools import reduce
@@ -11,7 +21,7 @@ out = []
 def print_row(*row):
     global out
 
-    print_str = "{}. host: {},  ip: {},  ping(ms): {},  location: {}- {},  lat_lng: {},  host: {},  org: {}, total_time_to_compute: {}".format(*row)
+    print_str = ("{}. host: "+bcolors.OKBLUE+"{}"+bcolors.ENDC+",  ip: "+bcolors.OKBLUE+"{}"+bcolors.ENDC+",  ping(ms): "+bcolors.HEADER+"{}"+bcolors.ENDC+",  location: {}- {},  lat_lng: {}, total_time_to_compute: {}ms").format(*row)
     out.append({"row":row, "print_str":print_str})
     print(print_str)
 
@@ -20,6 +30,10 @@ def finalize():
     print("--------------------\n------------------")
     print("The fastest server is: ")
     out.sort(key=lambda x:x['row'][3])
+
+    if len(out) == 0:
+        print(bcolors.FAIL,"NO HOST FOUND. IS YOUR INTERNET EVEN WORKING?", bcolors.ENDC)
+        return
 
     print(out[0]['print_str'])
     print("-----------------------------")
@@ -31,17 +45,21 @@ def finalize():
 
 os.chdir(SCRIPTS_DIR)
 
-ping_re_script = re.compile(r"time=(\d+\.*\d+) ms.*")
+ping_re_script = re.compile(r"time=(\d+\.*\d*)\s?ms.*")
 
 index = 0
-for f in glob.glob("*.tcp443.ovpn"):
+
+hosts = glob.glob("*.tcp443.ovpn")
+print("{} configs found".format(len(hosts)))
+
+for f in hosts:
     try:
-        host = '.'.join(f.split(".")[:3])
+        host = f.replace(".tcp443.ovpn", "")
 
         begin_time = time.time()
 
         ip = socket.gethostbyname(host)
-        
+
         location = requests.get("http://ipinfo.io/{}".format(ip)).text
         location = json.loads(location)
 
@@ -49,28 +67,38 @@ for f in glob.glob("*.tcp443.ovpn"):
             postal = location['postal']
         else:
             postal = ""
-        
-        provider = location['org']
+
         lat_lng = location['loc']
-        hostname = location['hostname']
 
         location = "{}, {}, {}".format(location['city'], location['region'], location['country'])
-        
 
-        _p = subprocess.Popen(['/bin/ping', "-c 4", "{}".format(ip)], stdout = subprocess.PIPE)
+        if os.name is 'nt': # windows
+            cmd = "ping.exe {}".format(ip)
+        else: # otherwise, we assume POSIX
+            cmd = "ping -c 4 -i 0.2 {}".format(ip) # -i for cross-request time 
+
+        _p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
         _p_resp = _p.communicate()[0]
         _p_resp = ping_re_script.findall(_p_resp.decode("UTF-8"))
+
+        if len(_p_resp) == 0: 
+            raise socket.gaierror;
+
         ping_resp = reduce(lambda x, y: float(x)+float(y), _p_resp) / len(_p_resp)
         ping_resp = int(ping_resp)
 
         end_time = time.time()
         time_diff = int((end_time - begin_time) * 1000)
-       
 
-        index += 1
-        print_row(index, host, ip, ping_resp, location, postal, lat_lng, hostname, provider, time_diff)
 
         if(index > 3000): break
-    except Exception:
-        print("Can not parse for {}".format(f))
+        index += 1
+        print_row(index, host, ip, ping_resp, location, postal, lat_lng, time_diff)
+    except socket.gaierror:
+        print(bcolors.WARNING,"host not found: {}".format(host), bcolors.ENDC)
+    except Exception as e:
+        print("Can not parse for {}".format(host), e)
+    except KeyboardInterrupt as e:
+        print("Keyboard interrupt, finalizing now.", e)
+        break
 finalize()
